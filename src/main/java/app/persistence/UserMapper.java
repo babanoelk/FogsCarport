b8da23600fb5ebc28e5251f3d4fb80794f4847a9
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2023-12-01 11:01:55 +0100

    UserMapper
    edited

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -3,15 +3,16 @@ package app.persistence;
 import app.entities.User;
 import app.exceptions.DatabaseException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserMapper {
-    public static void addUser(User newUser, ConnectionPool connectionPool) throws DatabaseException {
+    public static User addUser(User newUser, ConnectionPool connectionPool) throws DatabaseException {
+
+        User user = null;
+
         String sql = "INSERT INTO USER (name, email, password, address, zipcode, mobile) values (?,?,?,?,?,?)";
         try (Connection connection = connectionPool.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
                 //Number 0 is serialized ID
                 ps.setString(1, newUser.getName());
                 ps.setString(2, newUser.getEmail());
@@ -24,9 +25,45 @@ public class UserMapper {
                 if (rowsAffected != 1) {
                     throw new DatabaseException("Fejl i data leveret ved oprettelse af bruger i databasen");
                 }
+
+                ResultSet rs = ps.getGeneratedKeys();
+
+                int id = rs.getInt(1);
+
+                newUser.setId(id);
+
             }
         } catch (SQLException e) {
             throw new DatabaseException("Fejl ved oprettelse af bruger");
         }
+        return newUser;
     }
+
+    /*
+    public static User getUserIdByEmail(int userId, ConnectionPool connectionPool) throws DatabaseException{
+        User user = null;
+
+               String sql = "select * from USER where id = ?";
+
+               try(Connection connection = connectionPool.getConnection()){
+                   try(PreparedStatement ps = connection.prepareStatement(sql)){
+                       ResultSet rs = ps.executeQuery();
+                       int id = rs.getInt(1);
+                       String name = rs.getString(2);
+                       String email = rs.getString(3);
+                       String email = rs.getString(3);
+                       String email = rs.getString(3);
+
+
+                   }
+
+               }
+
+
+        return user;
+
+    }
+
+     */
+
 }

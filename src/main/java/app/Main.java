45fc1efe25e29d134b6fd47a4e96b543e11ac428
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2023-12-08 21:50:25 +0100

    email works as expected

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,27 +1,34 @@
 package app;
 
 import app.config.ThymeleafConfig;
-import app.controllers.FormController;
-import app.controllers.OrderController;
-import app.controllers.SystemController;
-import app.controllers.UserController;
+import app.controllers.*;
 import app.entities.Order;
 import app.persistence.ConnectionPool;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
+
+import java.io.IOException;
 
 public class Main {
     private static final String USER = "notactive";
     private static final String PASSWORD = "notactive";
     private static final String URL = "jdbc:postgresql://localhost:5432/notactive";
     private static final String DB = "notactive";
+    private static String API_KEY = System.getenv("SENDGRID_API_KEY");
+
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
     public static void main(String[] args)
     {
         // Initializing Javalin and Jetty webserver
-
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
 
@@ -41,6 +48,7 @@ public class Main {
         app.post("/se-order", ctx -> OrderController.getSpecificOrder(ctx,connectionPool));
         app.post("/opdater-ordre", ctx -> OrderController.updateOrderStatus(ctx, connectionPool));
         app.post("/kontakt", ctx -> OrderController.orderContact(ctx));
+        app.post("/besked-indsendt", ctx -> EmailController.sendOrderQuestion(ctx));
 
     }
 
--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -87,4 +87,7 @@ public class OrderController {
         ctx.render("kontakt.html");
     }
 
+
+
+
 }

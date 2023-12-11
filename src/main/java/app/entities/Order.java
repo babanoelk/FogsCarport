b493e1eeae4580e1010f66dffbc00cb6955def16
HanniSalman <hannisalman2510@outlook.com>
2023-12-11 13:14:39 +0100

    sd

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -6,10 +6,11 @@ public class Order {
     private int id;
     private Date date;
     private String customerNote;
-    //private boolean consent; Udkommenteret d. 11.12.2023
+    private boolean consent;
     private int userId;
     private String orderStatus; //this is an int in DB
     private int carportId;
+
 
     public Order(String customerNote) {
         this.customerNote = customerNote;
@@ -19,7 +20,7 @@ public class Order {
         this.date = date;
     }
 
-    /* Udkommenteret d. 11.12.2023
+
     public Order(int id, Date date, String customerNote, boolean consent, int userId, String orderStatus, int carportId) {
         this.id = id;
         this.date = date;
@@ -28,7 +29,7 @@ public class Order {
         this.userId = userId;
         this.orderStatus = orderStatus;
         this.carportId = carportId;
-    }*/
+    }
 
     public Order(int id, Date date, String customerNote, String orderStatus) {
         this.id = id;
@@ -49,53 +50,44 @@ public class Order {
         return date;
     }
 
-    /* Udkommenteret d. 11.12.2023
+
     public void setDate(Date date) {
         this.date = date;
-    }*/
+    }
 
     public String getCustomerNote() {
         return customerNote;
     }
 
-    /* Udkommenteret d. 11.12.2023
+
     public void setCustomerNote(String customerNote) {
         this.customerNote = customerNote;
-    }*/
-
-    public boolean getConsent() {
-        return consent;
     }
-
-    /* Udkommenteret d. 11.12.2023
-    public void setConsent(boolean consent) {
-        this.consent = consent;
-    }*/
 
     public int getUserId() {
         return userId;
     }
 
-    /* Udkommenteret d. 11.12.2023
+
     public void setUserId(int userId) {
         this.userId = userId;
-    }*/
+    }
 
     public String getOrderStatus() {
         return orderStatus;
     }
 
-    /* Udkommenteret d. 11.12.2023
+
     public void setOrderStatus(String orderStatus) {
         this.orderStatus = orderStatus;
-    }*/
+    }
 
     public int getCarportId() {
         return carportId;
     }
 
-    /* Udkommenteret d. 11.12.2023
+
     public void setCarportId(int carportId) {
         this.carportId = carportId;
-    }*/
+    }
 }
